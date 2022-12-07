69544a5e8f034c1a170bcd2ae21e202997087e9a
lohbrandt <102798720+lohbrandt@users.noreply.github.com>
2022-12-07 23:00:14 +0100

    3 files modified

--- src/app/book-list/book-list.component.ts
+++ src/app/book-list/book-list.component.ts
@@ -1,11 +1,24 @@
-import { Component } from "@angular/core";
-import { Book } from "../shared/book";
+import { Component, OnInit } from '@angular/core';
+import { Book } from '../shared/book';
 
 @Component({
-  selector: 'book-list',
-  styleUrls:['./book-list.component.css'],
-  templateUrl:'./book-list.component.html'
+  selector: 'bookList',
+  styleUrls: ['./book-list.component.css'],
+  templateUrl: './book-list.component.html',
 })
-export class BookListComponent{
- books: Book[]
+export class BookListComponent implements OnInit {
+  books: Book[];
+  ngOnInit(): void {
+    this.books = [
+      {
+        isbn: '12345678',
+        title: 'Angular',
+        authors: ['Ferdinand Malcher', 'Johannes Hoppe'],
+        published: new Date(2020,8,1),
+        subtitle: 'Grundlagen, fortgeschrittene Themen und Best Practices',
+        rating: 5,
+        thumbnails:[] 
+      }
+    ]
+  }
 }
